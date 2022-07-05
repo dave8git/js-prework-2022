d775ee8a67f99b69015a1a8033ad825f9579aea3
unknown <dave8git@gmail.com>
2022-07-05 13:19:12 +0200

    fix short if in move() function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 const playerNumber = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce');
 const computerNumber =  Math.floor(Math.random() * 3 + 1);
 function move(number) {
-    return number == 1 ? 'kamień' : (number == 2) ? 'papier' : (number == 3) ? 'nożyce': printMessage('Nie znam takiego ruchu');
+    return number == 1 ? 'kamień' : (number == 2) ? 'papier' : (number == 3) ? 'nożyce':  {return : printMessage('Nie znam takiego ruchu')};
 }
 let playerMove = move(playerNumber);
 let computerMove = move(computerNumber); 
-
-function whoWon(playerMove, computerMove) {
-  console.log(playerMove);
-  console.log(computerMove);
-  return ((playerMove == 'kamień' && computerMove == 'nożyce') || (playerMove == 'papier' && computerMove == 'kamień') || (playerMove =='kamień' && computerMove == 'nożyce')) ? printMessage('Wygrywasz!') : printMessage('komputer wygrywa');
-}
-
 whoWon(playerMove, computerMove);
+function whoWon(playerMove, computerMove) {
+  if ((playerMove == 'kamień' && computerMove == 'nożyce') || (playerMove == 'papier' && computerMove == 'kamień') || (playerMove =='kamień' && computerMove == 'nożyce')) {
+    printMessage('Wygrywasz!');
+  } else if (playerMove == computerMove) {
+    printMessage('Wygląda na to, że remis...')
+  } else {
+    printMessage('Skynet wygrywa... :( ');
+  }
+}
