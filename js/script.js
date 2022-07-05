3c34b785a91d68c3f9d3a4c323f27a992d2e7636
unknown <dave8git@gmail.com>
2022-07-05 14:57:40 +0200

    add game working

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 const playerNumber = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce');
 const computerNumber =  Math.floor(Math.random() * 3 + 1);
+
 function move(number) {
-    return number == 1 ? 'kamień' : (number == 2) ? 'papier' : (number == 3) ? 'nożyce':  {return : printMessage('Nie znam takiego ruchu')};
+  if(number >= 1 && number <= 3) {
+    if(number == 1) {
+      return 'kamień';
+    } else if (number == 2) {
+      return 'papier';
+    } else if (number == 3) {
+      return 'nożyce';
+    }
+  } else {
+    return printMessage('Nie znam takiego ruchu');
+  }
+    
 }
-let playerMove = move(playerNumber);
-let computerMove = move(computerNumber); 
-whoWon(playerMove, computerMove);
-function whoWon(playerMove, computerMove) {
-  if ((playerMove == 'kamień' && computerMove == 'nożyce') || (playerMove == 'papier' && computerMove == 'kamień') || (playerMove =='kamień' && computerMove == 'nożyce')) {
+
+let argPlayerMove = move(playerNumber);
+let argComputerMove = move(computerNumber); 
+
+console.log(argPlayerMove);
+console.log(argComputerMove);
+
+function whoWon(argPlayerMove, argComputerMove) {
+  if ((argPlayerMove == 'kamień' && argComputerMove == 'nożyce') || (argPlayerMove == 'papier' && argComputerMove == 'kamień') || (argPlayerMove =='kamień' && argComputerMove == 'nożyce')) {
     printMessage('Wygrywasz!');
-  } else if (playerMove == computerMove) {
+  } else if (argPlayerMove == argComputerMove) {
     printMessage('Wygląda na to, że remis...')
   } else {
     printMessage('Skynet wygrywa... :( ');
   }
 }
+
+whoWon(argPlayerMove, argComputerMove);
+ 
